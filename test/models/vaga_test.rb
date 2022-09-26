b1898fef763dd3f274256c9b34f6954d3feb7ea0
Fabricio Miranda <fbrnery@gmail.com>
2022-09-26 17:02:18 -0300

    Adcionei um Teste Unitário de Model

--- test/models/vaga_test.rb
+++ test/models/vaga_test.rb
@@ -1,6 +1,20 @@
 require "test_helper"
 
 class VagaTest < ActiveSupport::TestCase
+
+ test "vaga attributes must not be empty" do
+    vaga = Vaga.new
+    assert vaga.invalid?
+    assert vaga.errors[:empresa].any?
+    assert vaga.errors[:titulo].any?
+    assert vaga.errors[:localizacao].any?
+    assert vaga.errors[:contrato].any?
+    assert vaga.errors[:jornada].any?
+    assert vaga.errors[:local].any?
+    assert vaga.errors[:link].any?
+    assert vaga.errors[:descricao].any?
+  end
+
   # test "the truth" do
   #   assert true
   # end
